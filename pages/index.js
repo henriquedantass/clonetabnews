f98c2a801a8fac610f6cb83c8b4330d7937fbe08
[your name] <[your email]>
2023-08-15 12:05:40 -0300

    add first page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 function Home() {
-     return <h1>hello world</h1>
+     const [show, setShow] = useState(false);
+
+     return (
+          <div style={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
+               <h1>Olá Tayane, gostaria de saber o segredo?</h1>
+               <button onClick={() => setShow(!show)}>click para descobrir</button>
+               {show && (
+                    <>
+                    <h1>Você descobriu o jhon lombrado, parabéns!</h1>
+                    <img style={{width: '100%', height: 'auto'}} src="jhon-lombrado.jpeg" />
+                    </>
+               )}
+          </div>
+     )
 }
 
 export default Home;
